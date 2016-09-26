2dd72f23190f3f45692e84c3542d89d4c41e9349
keunsulee <i.knsulee@gmail.com>
2016-09-26 14:14:04 +0900

    ...

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -42,6 +43,18 @@ public final class PetContract {
     public static final class PetEntry implements BaseColumns {
 
         /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        /**
          * The content URI to access the pet data in the provider
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
@@ -53,38 +66,38 @@ public final class PetContract {
 
         /**
          * Unique ID number for the pet (only for use in the database table).
-         * <p>
+         * <p/>
          * Type: INTEGER
          */
         public final static String _ID = BaseColumns._ID;
 
         /**
          * Name of the pet.
-         * <p>
+         * <p/>
          * Type: TEXT
          */
         public final static String COLUMN_PET_NAME = "name";
 
         /**
          * Breed of the pet.
-         * <p>
+         * <p/>
          * Type: TEXT
          */
         public final static String COLUMN_PET_BREED = "breed";
 
         /**
          * Gender of the pet.
-         * <p>
+         * <p/>
          * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
          * or {@link #GENDER_FEMALE}.
-         * <p>
+         * <p/>
          * Type: INTEGER
          */
         public final static String COLUMN_PET_GENDER = "gender";
 
         /**
          * Weight of the pet.
-         * <p>
+         * <p/>
          * Type: INTEGER
          */
         public final static String COLUMN_PET_WEIGHT = "weight";
--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -189,7 +189,6 @@ public class PetProvider extends ContentProvider {
      * Update pets in the database with the given content values. Apply the changes to the rows
      * specified in the selection and selection arguments (which could be 0 or 1 or more pets.)
      * Return the number of rows that were successfully updated.
-     *
      */
     private int updatePet(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
 
@@ -218,7 +217,22 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+            case PET_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PetEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     /**
@@ -226,7 +240,15 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return PetEntry.CONTENT_LIST_TYPE;
+            case PET_ID:
+                return PetEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 }
 
